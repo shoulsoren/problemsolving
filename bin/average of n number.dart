b325febab0becf9b7be3afb58f16dc77a2e7afba
shoulsoren <shuvosoren1994@gmail.com>
2024-03-11 17:01:43 +0600

    Initial commit

---
+++ bin/average of n number.dart
@@ -0,0 +1,21 @@
+
+//write a program that find average of n number in dart
+
+import 'dart:io';
+
+void main(){
+  print("How main numbers would you like to average?");
+  int n = int.parse(stdin.readLineSync()!);
+
+  double sum = 0;
+
+  for (int i = 0; i < n; i++){
+    print("Enter number ${i+1}:");
+    double num = double.parse(stdin.readLineSync()!);
+    sum += num;
+  }
+double average = sum / n;
+
+  print("The average of the $n number is $average");
+
+}
---
+++ bin/vowel or consonant.dart
@@ -0,0 +1,18 @@
+//Check Whether an alphabet is a vowel or consonant
+
+
+import 'dart:io';
+
+void main(){
+  print("Enter a letter: ");
+  String letter=stdin.readLineSync()!;
+
+  if (letter=='a' || letter=='e' || letter=='i' || letter=='o' || letter=='u' ||
+      letter=='A' || letter=='E' || letter=='I' || letter=='O' || letter=='U'){
+
+    print("$letter is vowel.");
+  } else {
+    print("$letter is consonant.");
+  }
+
+}
